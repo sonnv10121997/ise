25acd83b80c7f35027619c767fdf5f5a2e2023d5
sonnv10121997 <sonnv10121997@gmail.com>
2019-03-04 13:07:08 +0700

    add i18n gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i(name gender code dob phone)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
