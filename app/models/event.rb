63bf2abf1fafaf983de988b47d0fe53c58faabcf
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-03-07 13:57:01 +0700

    Add event view, controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :max_participants, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :semester, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users, module: :users, skip: :omniauth_callbacks
-
     root "events#index"
   end
 end
