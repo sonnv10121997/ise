ec9fe35daacc2c522a25c81be74d0714c8d93d8c
sonnv10121997 <sonnv10121997@gmail.com>
2019-03-15 22:22:32 +0700

    fix header

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :find_event, except: %i(index new create)
+  before_action :find_event, only: %i(show)
 
   def index
     @events = Event.all
@@ -7,51 +7,9 @@ class EventsController < ApplicationController
 
   def show; end
 
-  def new
-    @event = Event.new
-  end
-
-  def edit; end
-
-  def create
-    @event = Event.new(event_params)
-
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
-    end
-  end
-
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-    end
-  end
-
   private
 
   def find_event
-    @event = Event.find params[:id]
-  end
-
-  def event_params
-    params.require(:event)
-        .permit :name, :description, :price,
-                :max_participants, :joined_participants,
-                :start_date, :end_date, :semester
+    @event = Event.find_by id: params[:id]
   end
 end
