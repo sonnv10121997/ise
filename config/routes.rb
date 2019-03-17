b7aef76a5517f5d2c50aedeceb3759e6b9d66182
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-03-17 14:28:29 +0700

    Handle reset password

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users, module: :users, skip: :omniauth_callbacks
+    resources :users, only: :show
     resources :events, only: %i(index show)
     root "events#index"
   end
