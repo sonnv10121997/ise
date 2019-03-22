8a8ebec1f22c2eeab027166c7c8d03dd54471284
sonnv10121997 <sonnv10121997@gmail.com>
2019-03-22 16:07:16 +0700

    add ransack and searches_controller

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,8 @@
+class SearchesController < ApplicationController
+  def index
+    q = params[:q]
+    events = Event.ransack(name_cont_all: q).result distinct: true
+    partners = Partner.ransack(name_cont_all: q).result distinct: true
+    @items = events + partners
+  end
+end
