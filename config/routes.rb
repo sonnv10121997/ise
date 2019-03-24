de846bc808dcebc013b1a8434e42bfc090a58e50
sonnv10121997 <sonnv10121997@gmail.com>
2019-03-24 23:31:53 +0700

    add requirements checkable

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
     resources :users, only: :show
     resources :events, only: %i(index show)
     resources :user_enroll_events, only: %i(show create destroy), param: :event_id
-    resources :user_event_requirements, only: :update
+    resources :user_event_requirements, only: %i(upload_image check_requirement) do
+      member do
+        patch "/", to: "user_event_requirements#upload_image"
+        put "/", to: "user_event_requirements#check_requirement"
+      end
+    end
     resources :searches, only: :index
     resources :partners, only: :show
     resources :conversations, only: :create do
