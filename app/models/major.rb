665b04a0bc9597685e2e4b31e8d4bf9bb75c2142
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-04-11 10:22:41 +0700

    Validate add major

--- app/models/major.rb
+++ app/models/major.rb
@@ -3,4 +3,6 @@ class Major < ApplicationRecord
   has_many :events, through: :event_majors
 
   validates_presence_of :name, :acronym
+  validates :name, format: {with: Regexp.new(Settings.regex.only_word)}
+  validates :acronym, format: {with: Regexp.new(Settings.regex.upper_case)}
 end
