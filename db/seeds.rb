4e9b63c927b26320b70ac325a8652b9246fc8245
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-04-13 16:05:00 +0700

    Fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,13 @@
-File.open("app/uploaders/avatar.jpg") do |file|
-  User.types.keys.each_with_index do |type, index|
-    name = index.zero? ? "Administrator" : type
-    code = ('A'..'Z').to_a.shuffle[0, 2].join << (0..9).to_a.shuffle[0, 8].join
-    user = User.new
-    user.build_avatar.file = file
-    user.update_attributes name: name, email: "#{type.downcase}@fpt.edu.vn",
-      type: type, password: "123456", gender: (0..1).to_a.shuffle[0],
-      code: code, phone: (0..9).to_a.shuffle[0, 10].join, dob: Date.today,
-      confirmed_at: Date.today
-    user.save!
-  end
+User.types.keys.each_with_index do |type, index|
+  name = index.zero? ? "Administrator" : type
+  code = ('A'..'Z').to_a.shuffle[0, 2].join << (0..9).to_a.shuffle[0, 8].join
+  user = User.new
+  user.build_avatar.file = file
+  user.update_attributes name: name, email: "#{type.downcase}@fpt.edu.vn",
+    type: type, password: "123456", gender: (0..1).to_a.shuffle[0],
+    code: code, phone: (0..9).to_a.shuffle[0, 10].join, dob: Date.today,
+    confirmed_at: Date.today
+  user.save!
 end
 
 Major.create! name: "English", acronym: "EN"
