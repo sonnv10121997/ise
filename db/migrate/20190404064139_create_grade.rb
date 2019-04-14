f97708bfffd39c5496a7518bd3f5a55a010aff2c
hacunamatata97 <qwerty.lynkaa@gmail.com>
2019-04-14 10:20:28 +0700

    Fix null in new user avatar

--- db/migrate/20190404064139_create_grade.rb
+++ db/migrate/20190404064139_create_grade.rb
@@ -3,7 +3,7 @@ class CreateGrade < ActiveRecord::Migration[5.2]
     create_table :grades do |t|
       t.string :name, null: false
       t.float :weight, null: false
-      t.float :value, null: false
+      t.float :value
       t.references :grade_category, foreign_key: true, null: false
 
       t.timestamps
