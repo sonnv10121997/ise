59686e34b88776a2fcf8f17d26f999b51db132ed
sonnv10121997 <sonnv10121997@gmail.com>
2019-05-02 15:46:42 +0700

    fix real-time requirement

--- app/assets/javascripts/requirement.js
+++ app/assets/javascripts/requirement.js
@@ -1,20 +1,20 @@
 $(document).on(`turbolinks:load`, function () {
   var eventId = $(`#event_id`).val();
-  var currentUserId = $(`#current_user_id`).val();
+  var participantId = $(`#participant_id`).val();
 
   App.message = App.cable.subscriptions.create({
     channel: `RequirementChannel`,
-    event_id: eventId, user_id: currentUserId}, {
+    event_id: eventId, participant_id: participantId}, {
     connected: function () { },
     received: function (data) {
       if (data.requirement !== undefined) {
         var requirement = data.requirement;
-        var reqUserIsCurrentUser = (currentUserId == requirement.user_id);
+        var isParticipant = (participantId == requirement.user_id);
 
-        if (data.method == `check_requirement` && reqUserIsCurrentUser) {
+        if (data.method == `check_requirement` && isParticipant) {
           $(`#requirement_${requirement.id}`).find(`#status`)
             .replaceWith(requirement.status);
-        } else if (data.method == `upload_image` && !reqUserIsCurrentUser) {
+        } else if (data.method == `upload_image` && isParticipant) {
           $(`#requirement_${requirement.id}`).find(`#images`)
             .html(requirement.image);
         }
