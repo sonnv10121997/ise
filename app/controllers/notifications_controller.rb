65abfcd3d2545f9257f5533fd188d64d766f1a52
sonnv10121997 <sonnv10121997@gmail.com>
2019-05-08 18:39:20 +0700

    add unenroll request button

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,16 @@
 class NotificationsController < ApplicationController
+  attr_reader :event
+
+  before_action ->{find_event params[:event_id]}, only: :create
+
   def index
     @notifications = current_user.notifications.page(params[:page])
       .per Settings.model.notification.list
+  end
+
+  def create
+    create_notification event, nil, nil, current_user, event.leader,
+      Notification.types.values[10], "warning"
   end
 
   def update
